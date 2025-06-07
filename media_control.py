e677a54587d96d032dddfdd6bf95db20f3cc3b11
Gosheto1234 <104501999+Gosheto1234@users.noreply.github.com>
2025-06-07 19:19:02 +0300

    Update media_control.py

--- media_control.py
+++ media_control.py
@@ -1,43 +1,71 @@
 # media_control.py
 
-import asyncio
-from winrt.windows.media.control import (
+from winsdk.windows.media.control import (
     GlobalSystemMediaTransportControlsSessionManager
 )
 
 class MediaController:
     def __init__(self):
-        # wrap the WinRT async call in a coroutine
-        async def _get_mgr():
-            return await GlobalSystemMediaTransportControlsSessionManager.request_async()
+        # don’t call request_async() yet
+        self._mgr = None
+        self._session = None
 
-        # drive it to completion
-        self._mgr = asyncio.run(_get_mgr())
+    def _ensure_mgr(self):
+        if not self._mgr:
+            try:
+                op = GlobalSystemMediaTransportControlsSessionManager.request_async()
+                self._mgr = op.get_results()
+            except OSError as e:
+                # “method called at unexpected time” → defer until next call
+                self._mgr = None
+                raise
 
     def _update_session(self):
+        self._ensure_mgr()
         self._session = self._mgr.get_current_session()
 
     def play(self):
-        self._update_session()
-        if self._session:
-            asyncio.run(self._session.try_play_async())
+        try:
+            self._update_session()
+            if self._session:
+                op = self._session.try_play_async()
+                op.get_results()
+        except OSError:
+            # if we hit it again, we’ll just swallow and retry next time
+            pass
 
     def pause(self):
-        self._update_session()
-        if self._session:
-            asyncio.run(self._session.try_pause_async())
+        try:
+            self._update_session()
+            if self._session:
+                op = self._session.try_pause_async()
+                op.get_results()
+        except OSError:
+            pass
 
     def stop(self):
-        self._update_session()
-        if self._session:
-            asyncio.run(self._session.try_stop_async())
+        try:
+            self._update_session()
+            if self._session:
+                op = self._session.try_stop_async()
+                op.get_results()
+        except OSError:
+            pass
 
     def next(self):
-        self._update_session()
-        if self._session:
-            asyncio.run(self._session.try_skip_next_async())
+        try:
+            self._update_session()
+            if self._session:
+                op = self._session.try_skip_next_async()
+                op.get_results()
+        except OSError:
+            pass
 
     def previous(self):
-        self._update_session()
-        if self._session:
-            asyncio.run(self._session.try_skip_previous_async())
+        try:
+            self._update_session()
+            if self._session:
+                op = self._session.try_skip_previous_async()
+                op.get_results()
+        except OSError:
+            pass
